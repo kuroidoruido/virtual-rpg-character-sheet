d224d272c1df3027d5ba231c1c549c19375331c7
Anthony Pena <anthony.pena@outlook.fr>
2020-04-04 11:40:13 +0200

    feature - basic poc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewChecked, ViewChild } from '@angular/core';
+
+import cthulhuV7frClassique from '../assets/cthulhu-v7-fr-classique/config.json';
+
+type Percent = number;
+
+interface SheetDataPosition {
+  key: string;
+  x: number;
+  y: number;
+  width: Percent;
+  value: string | number;
+  fontSize?: Percent;
+}
+
+interface SheetData {
+  content: {
+    page1: SheetDataPosition[],
+    page2: SheetDataPosition[],
+  };
+  pageConfig: {
+    pageFormat: 'A4';// ! TODO fix ratio bug (ratio is not respected if this value is under 100)
+    pageWidth: Percent;
+    pages: string[];
+  };
+}
+
+const LOCAL_STORAGE_SHEET_DATA_KEY = "VirtualRgpCharacterSheet.SheetData.";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'virtual-rpg-character-sheet';
+export class AppComponent implements AfterViewChecked {
+  @ViewChild('pages', { static: true }) pagesWrapperElement: any;
+
+  sheetData: SheetData = cthulhuV7frClassique as SheetData;
+  game = 'cthulhu-v7-fr-classique';
+
+  constructor() {
+    const saved = localStorage.getItem(`${LOCAL_STORAGE_SHEET_DATA_KEY}${this.game}`);
+    if(saved !== undefined && saved !== null) {
+      this.sheetData = JSON.parse(saved);
+    }
+  }
+
+  ngAfterViewChecked() {
+    if(this.pagesWrapperElement !== undefined && this.pagesWrapperElement.nativeElement !== undefined) {
+      this.pagesWrapperElement.nativeElement.style.setProperty('--pageWidth', `${ this.sheetData.pageConfig.pageWidth / 2 }%`);
+    }
+  }
+
+  save() {
+    localStorage.setItem(`${LOCAL_STORAGE_SHEET_DATA_KEY}${this.game}`, JSON.stringify(this.sheetData));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatSliderModule } from '@angular/material/slider';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
-    BrowserAnimationsModule
+    
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    
+    MatButtonModule,
+    MatSliderModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
