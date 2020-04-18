830888e478e3f72b203120346a6ea29205800ecd
Anthony Pena <anthony.pena@outlook.fr>
2020-04-18 21:57:43 +0200

    refactor - local storage keys

--- src/app/shared/save/save.service.ts
+++ src/app/shared/save/save.service.ts
@@ -3,8 +3,10 @@ import { Injectable } from '@angular/core';
 import { CharacterData } from 'src/app/model/character-data.model';
 import { Observable, ReplaySubject, Subject } from 'rxjs';
 
-const LOCAL_STORAGE_SHEET_DATA_KEY = "VirtualRgpCharacterSheet.CharacterData.";
-const LOCAL_STORAGE_NOTE_DATA_KEY = "VirtualRgpCharacterSheet.notes.";
+const LOCAL_STORAGE_KEY_FRAGMENT_SEPARATOR = '.';
+const LOCAL_STORAGE_BASE_KEY = 'VirtualRgpCharacterSheet';
+const LOCAL_STORAGE_SHEET_DATA_KEY = 'characterData';
+const LOCAL_STORAGE_NOTE_DATA_KEY = 'notes';
 
 type SaveState = 'idle' | 'saving' | 'saved';
 
@@ -19,20 +21,26 @@ export class SaveService {
     this.saveState$.next('idle');
   }
 
+  private getStorageKey(game: string, typeKey: string): string {
+    return LOCAL_STORAGE_BASE_KEY 
+      + LOCAL_STORAGE_KEY_FRAGMENT_SEPARATOR + game 
+      + LOCAL_STORAGE_KEY_FRAGMENT_SEPARATOR +  typeKey;
+  }
+
   saveState(): Observable<SaveState> {
     return this.saveState$.asObservable();
   }
 
   save(game: string, characterData: CharacterData) {
     this.saveState$.next('saving');
-    localStorage.setItem(`${LOCAL_STORAGE_SHEET_DATA_KEY}${game}`, JSON.stringify(characterData));
+    localStorage.setItem(this.getStorageKey(game, LOCAL_STORAGE_SHEET_DATA_KEY), JSON.stringify(characterData));
     this.saveState$.next('saved');
     // setTimeout is for let user see saved info
     setTimeout(() => this.saveState$.next('idle'), 2000);
   }
 
   restore(game: string): CharacterData | undefined {
-    const saved = localStorage.getItem(`${LOCAL_STORAGE_SHEET_DATA_KEY}${game}`);
+    const saved = localStorage.getItem(this.getStorageKey(game, LOCAL_STORAGE_SHEET_DATA_KEY));
     if(saved !== undefined && saved !== null) {
       return JSON.parse(saved);
     }
@@ -41,14 +49,14 @@ export class SaveService {
 
   saveNotes(game: string, notes: string) {
     this.saveState$.next('saving');
-    localStorage.setItem(`${LOCAL_STORAGE_NOTE_DATA_KEY}${game}`, JSON.stringify(notes.split('\n')));
+    localStorage.setItem(this.getStorageKey(game, LOCAL_STORAGE_NOTE_DATA_KEY), JSON.stringify(notes.split('\n')));
     this.saveState$.next('saved');
     // setTimeout is for let user see saved info
     setTimeout(() => this.saveState$.next('idle'), 2000);
   }
 
   restoreNotes(game: string): string | undefined {
-    const saved = localStorage.getItem(`${LOCAL_STORAGE_NOTE_DATA_KEY}${game}`);
+    const saved = localStorage.getItem(this.getStorageKey(game, LOCAL_STORAGE_NOTE_DATA_KEY));
     if(saved !== undefined && saved !== null) {
       return JSON.parse(saved).join('\n');
     }
