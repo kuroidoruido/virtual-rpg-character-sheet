1f41c84058c52fdd3a8f7249f14e3381332f1093
Nick <handnf@gmail.com>
2021-10-20 11:03:46 -0400

    Move downloading to use Save Service, gameSheetId

--- src/app/sheet-selector/sheet-card/sheet-card.component.ts
+++ src/app/sheet-selector/sheet-card/sheet-card.component.ts
@@ -3,6 +3,7 @@ import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { SheetWithUserInfos, GameWithUserInfos, Character } from 'src/app/model/game.model';
+import { SaveService } from 'src/app/shared/save/save.service';
 
 @Component({
   selector: 'app-sheet-card',
@@ -13,20 +14,32 @@ export class SheetCardComponent {
   @Input() game: GameWithUserInfos | undefined;
   @Input() sheet: SheetWithUserInfos | undefined;
 
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    private saveService: SaveService,
+  ) {}
+
+  /**
+   * A concatenation of the game ID and sheet ID representing a specific
+   * character sheet. Used for both app routing and interfacing with the
+   * Save Service.
+   */
+  get gameSheetId() {
+    return `${this.game.gameId}.${this.sheet.sheetId}`;
+  }
 
   openCharacterSheet(character: Character) {
-    this.router.navigate([this.game.gameId + '.' + this.sheet.sheetId]);
+    this.router.navigate([this.gameSheetId]);
     // TODO remove this when we will be able to have multiple characters
     console.log('character selected', character);
   }
 
   createCharacterSheet() {
-    this.router.navigate([this.game.gameId + '.' + this.sheet.sheetId]);
+    this.router.navigate([this.gameSheetId]);
   }
 
   downloadVrpgJson() {
-    const fileData = JSON.stringify(this.sheet.characters);
+    const fileData = JSON.stringify(this.saveService.restore(this.gameSheetId));
     const blob = new Blob([fileData], {type: 'application/json'});
     const url = URL.createObjectURL(blob);
     const aElement = document.createElement('a');
